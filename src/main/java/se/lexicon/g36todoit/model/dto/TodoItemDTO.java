6704402fc97076a9aa8a3dd9a0be6afa1f2abe9d
Erik Svensson <eosvensson@gmail.com>
2021-08-13 15:20:52 +0200

    Added simple validation to TodoItemDTO

--- src/main/java/se/lexicon/g36todoit/model/dto/TodoItemDTO.java
+++ src/main/java/se/lexicon/g36todoit/model/dto/TodoItemDTO.java
@@ -2,13 +2,23 @@ package se.lexicon.g36todoit.model.dto;
 
 import se.lexicon.g36todoit.model.entity.Person;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 public class TodoItemDTO implements Serializable {
     private Integer id;
+    @NotBlank(message = "This field is mandatory")
+    @Size(min = 2, max = 50, message = "Title need to be between 2 and 50 letters long")
     private String title;
+    @NotBlank(message = "This field is mandatory")
+    @Size(min = 2, max = 1000, message = "Description need to have at least 2 letters")
     private String description;
+    @NotNull(message = "This field is mandatory")
+    @FutureOrPresent(message = "Dead line need to be in present or future")
     private LocalDate deadLine;
     private boolean done;
     private Person assignee;
