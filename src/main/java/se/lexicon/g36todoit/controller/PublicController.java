286923a50cecb35940be587f72e62ec0b26f58f3
Erik Svensson <eosvensson@gmail.com>
2021-08-16 14:27:45 +0200

    Added index.html and navbar.html

---
+++ src/main/java/se/lexicon/g36todoit/controller/PublicController.java
@@ -0,0 +1,13 @@
+package se.lexicon.g36todoit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublicController {
+
+    @GetMapping(value = {"", "/", "/index"})
+    public String loadWelcome(){
+        return "index";
+    }
+}
